a5701bd6a0c195a4f285a4b173ab6475952d6287
Kyle DAmato <kyledamato4@gmail.com>
2022-11-17 21:26:35 -0500

    finished structure of homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import About from "./pages/About"
+import Home from "./pages/Home"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <About />
+      <Home />
+      {/* <About /> */}
     </div>
   );
 }
