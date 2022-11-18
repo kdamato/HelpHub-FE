37153f5d40b1b96bd01e8d6b33d3051809194b39
Kyle DAmato <kyledamato4@gmail.com>
2022-11-17 22:13:35 -0500

    created structure for both login pages and routing within App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import React, {useEffect, useState} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About"
 import Home from "./pages/Home"
+import LoginCustomer from "./pages/LoginCustomer"
+import LoginProvider from "./pages/LoginProvider"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      {/* <About /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <Home />
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <About />
+            }
+          />
+          <Route path="/customer-login" element={<LoginCustomer />} />
+          <Route
+            path="/customer-login"
+            element={
+              <LoginCustomer />
+            }
+          />
+          <Route
+            path="/provider-login"
+            element={
+              <LoginProvider />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/LoginCustomer.js
+++ src/pages/LoginCustomer.js
@@ -1,15 +1,43 @@
 import Container from "react-bootstrap/Container";
 import Navigation from "../components/Navigation";
 import Stack from "react-bootstrap/Stack";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useNavigate } from 'react-router-dom'
 
 
 function LoginCustomer() {
+  const navigate = useNavigate()
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    navigate('/home')
+  }
+
+
   return (
     <div>
       <Container>
         <Stack gap={3}>
           <Navigation />
         </Stack>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="text" placeholder="Customer Email" />
+            <Form.Text className="text-muted">
+                Please enter your email here.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formGroupPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="text" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+    </Form>
       </Container>
     </div>
   );
