88fb79155743086e7011e44ec011b0d136438af6
unknown <kylelarsen12@gmail.com>
2022-11-19 14:01:07 -0500

    Start work connecting front+backends and user authentication

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import './App.css';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import "./App.css";
+import CurrentUserProvider from "./context/CurrentUser";
 
 function App() {
   return (
-    <div className="App">
-
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Home />
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <About />
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/login"
-            element={
-              <Login />
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <CurrentUserProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </CurrentUserProvider>
   );
 }
 
--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,27 +1,75 @@
 import Container from "react-bootstrap/Container";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useState } from "react"
-import { useNavigate } from 'react-router-dom'
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import { CurrentUser } from "../context/CurrentUser";
 
 function SubmitForm(props) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { setCurrentUser } = useContext(CurrentUser);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    navigate('/')
-  }
+  const handleSubmit = async (e) => {
+    //LOGIN
+    if (props.route === "login") {
+      e.preventDefault();
+      const response = await fetch(
+        "http://localhost:5050/memberAccounts/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
+        }
+      );
+      const data = await response.json();
 
-  const [title, setTitle] = useState('User type')
-  const handleSelection = event => {
+      if (response.status === 200) {
+        setCurrentUser(data.user);
+        localStorage.setItem("token", data.token);
+        navigate("/");
+      } else {
+        setErrorMessage(data.message);
+      }
+    }
+
+    //SIGNUP
+    else if (props.route === "signup") {
+      e.preventDefault();
+      const response = await fetch("http://localhost:5050/memberAccounts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
+      const data = await response.json();
+
+      if (response.status === 200) {
+        setCurrentUser(data.user);
+        localStorage.setItem("token", data.token);
+        navigate("/");
+      } else {
+        setErrorMessage(data.message);
+      }
+    }
+  };
+
+  //Update dropdown button display based on selection
+  const [title, setTitle] = useState("User type");
+  const handleSelection = (event) => {
     event.preventDefault();
-    setTitle(event.target.textContent)
-  }
+    setTitle(event.target.textContent);
+  };
 
   return (
     <div>
@@ -31,30 +79,32 @@ function SubmitForm(props) {
             <Form.Label>Email</Form.Label>
             <Form.Control type="text" placeholder="Email" />
             <Form.Text className="text-muted">
-                Please enter your email here.
+              Please enter your email here.
             </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formGroupPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
-                <DropdownButton
-                  as={ButtonGroup}
-                  key={'Primary'}
-                  id={`dropdown-Primarys-Primary`}
-                  title={title}
-                >
-                  <Dropdown.Item  onClick={handleSelection}> Customer </Dropdown.Item>
-                  <Dropdown.Item  onClick={handleSelection}> Helper </Dropdown.Item>
-                </DropdownButton>
+            <DropdownButton
+              as={ButtonGroup}
+              key={"Primary"}
+              id={`dropdown-Primarys-Primary`}
+              title={title}
+            >
+              <Dropdown.Item onClick={handleSelection}>
+                {" "}
+                Customer{" "}
+              </Dropdown.Item>
+              <Dropdown.Item onClick={handleSelection}> Helper </Dropdown.Item>
+            </DropdownButton>
           </Form.Group>
           <Button variant="primary" type="submit">
             {props.route}
           </Button>
-    </Form>
+        </Form>
       </Container>
     </div>
   );
-
 }
 export default SubmitForm;
