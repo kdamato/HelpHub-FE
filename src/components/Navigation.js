e28bba1c5924b3bd1af8cd105ae249d3634bac75
unknown <kylelarsen12@gmail.com>
2022-11-21 23:35:40 -0500

    Work on user login authentication

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,27 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { useContext } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { CurrentUser } from "../context/CurrentUser";
 
 function Navigation() {
+  const { currentUser } = useContext(CurrentUser);
+  // let loginActions = (
+  //   <>
+  //     <Nav.Link href="/login">Log in</Nav.Link>
+  //   </>
+  // );
+  // if (currentUser) {
+  //   let loginActions = <p>Logged in as {currentUser.email}</p>;
+  // }
+
+  const loginActions = currentUser ? (
+    !(<p>Logged in as {currentUser.email}</p>)
+  ) : (
+    <>
+      <Nav.Link href="/login">Log in</Nav.Link>
+    </>
+  );
   return (
     <>
       <Navbar bg="light" variant="light">
@@ -12,7 +31,7 @@ function Navigation() {
             <Nav.Link href="/about">About Us</Nav.Link>
             <Nav.Link href="/provider-signup">Sign Up Provider</Nav.Link>
             <Nav.Link href="/customer-signup">Sign up Customer</Nav.Link>
-            <Nav.Link href="/login">Log in</Nav.Link>
+            {loginActions}
           </Nav>
         </Container>
       </Navbar>
