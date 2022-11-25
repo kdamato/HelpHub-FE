de44bb06a0801e083a5a36d016f2fa4c3d7fb686
unknown <kylelarsen12@gmail.com>
2022-11-25 17:14:27 -0500

    debugging for jwt and current user

--- src/context/CurrentUser.js
+++ src/context/CurrentUser.js
@@ -3,15 +3,18 @@ import { createContext, useState, useEffect } from "react";
 export const CurrentUser = createContext();
 
 function CurrentUserProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState({});
+  const [currentUser, setCurrentUser] = useState(null);
   console.log(currentUser);
   useEffect(() => {
     const getLoggedInUser = async () => {
-      let response = await fetch(`http://localhost:5050/memberAccounts/:id`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
+      let response = await fetch(
+        `http://localhost:5050/memberAccounts/${currentUser._id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
       let user = await response.json();
       setCurrentUser(user);
       console.log(user);
