24cc9c337a0d398bd4c9956f637bb636038b41f2
Kyle DAmato <kyledamato4@gmail.com>
2022-11-27 12:56:06 -0500

    tweaking css, found why images weren't rendering properly and working on a fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Navigation from "../components/Navigation";
 import Stack from "react-bootstrap/Stack";
-import HomeCleaning from "../images/HomeCleaningHH.jpg";
-import LandScaping from "../images/landscapingHH.jpg";
-import PetCare from "../images/PetCareHH.jpg";
-import Moving from "../images/MovingHH.jpg";
-// import "../homepage.css";
+// import HomeCleaning from "../images/HomeCleaningHH.png";
+// import LandScaping from "../images/LandscapingHH.png";
+// import PetCare from "../images/PetCareHH.png";
+// import Moving from "../images/MovingHH.png";
+import clean from "../images/clean.png";
+import dog from "../images/dog.png";
+import move from "../images/move.png";
+import mow from "../images/mow.png";
+
 
 
 function Home(props) {
@@ -17,10 +21,14 @@ function Home(props) {
         </Stack>
     <div>
     <div className="top-Home-Page">
-          <img src={LandScaping} alt="Landscaping" id="landscaping" />
+          {/* <img src={LandScaping} alt="Landscaping" id="landscaping" />
           <img src={HomeCleaning} alt="Home Cleaning" id="homecleaning" />
           <img src={PetCare} alt="Landscaping" id="petcare" />
-          <img src={Moving} alt="Landscaping" id="moving" />
+          <img src={Moving} alt="Landscaping" id="moving" /> */}
+          <img src={clean} alt="clean" id="clean" />
+          <img src={dog} alt="dog" id="dog" />
+          <img src={move} alt="move" id="move" />
+          <img src={mow} alt="mow" id="mow" />
           <h1 className="Title">Select a Search</h1>
           <div class="form-group">
               <label for=""></label>
