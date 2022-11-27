48835f83ab99169f49ed70b329bb5975b266d1d4
unknown <kylelarsen12@gmail.com>
2022-11-27 01:13:49 -0500

    Update navbar with new endpoints

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import MyJobs from "./pages/MyJobs";
-import Jobs from "./pages/Jobs"
-import './App.css';
+import Jobs from "./pages/Jobs";
+import "./App.css";
 import SignUp from "./pages/SignUp";
 import CurrentUserProvider from "./context/CurrentUser";
-import Profile from "./pages/Profile"
-
+import Profile from "./pages/Profile";
+import NewJobForm from "./pages/NewJobForm";
 
 function App() {
   return (
@@ -19,11 +19,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/signup" element={<SignUp />} /> 
+            <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/searchjobs" element={<Jobs />}/>
-            <Route path="/myjobs" element={<MyJobs />}/>
+            <Route path="/searchjobs" element={<Jobs />} />
+            <Route path="/myjobs" element={<MyJobs />} />
+            <Route path="/newjob" element={<NewJobForm />} />
           </Routes>
         </BrowserRouter>
       </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,8 +10,8 @@ function Navigation() {
     <Nav.Link href="/profile">Welcome, {currentUser.name}</Nav.Link>
   ) : (
     <>
-      <Nav.Link href="/login">Log in</Nav.Link>
       <Nav.Link href="/signup">Sign Up</Nav.Link>
+      <Nav.Link href="/login">Log in</Nav.Link>
     </>
   );
   return (
@@ -21,6 +21,9 @@ function Navigation() {
           <Navbar.Brand href="/">HelpHub</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="/myjobs">My Listings</Nav.Link>
+            <Nav.Link href="/newjob">Post a Job</Nav.Link>
+            <Nav.Link href="/searchjobs">Look for a Job</Nav.Link>
             {loginActions}
           </Nav>
         </Container>
