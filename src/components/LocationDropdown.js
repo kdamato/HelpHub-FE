f61e88b59fe6eed4de3c5a86bd9c4245ff0d7062
unknown <kylelarsen12@gmail.com>
2022-11-27 23:04:08 -0500

    Update dropdowns to select components, prop drill so location comes through on forms. Need to finish sending postedBy for jobform

--- src/components/LocationDropdown.js
+++ src/components/LocationDropdown.js
@@ -1,83 +1,43 @@
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Form from "react-bootstrap/Form";
 import { useState } from "react";
 
-function LocationDropdown() {
-    //Update dropdown button display based on selection
-    const [title, setTitle] = useState("Select Location");
-    const handleSelection = (event) => {
-        event.preventDefault();
-        setTitle(event.target.textContent);
-    };
+function LocationDropdown(props) {
+  //Update dropdown button display based on selection
+  const [title, setTitle] = useState("Select Location");
 
+  //prettier-ignore
+  const states = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD", "MA", "MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"];
+
+  const mapStates = states.map((state, i) => {
+    return (
+      <option value={state} key={i}>
+        {state}
+      </option>
+    );
+  });
 
   return (
-    <>
-        <div as={ButtonGroup}>
-          <DropdownButton
-            as={ButtonGroup}
-            key={"Primary"}
-            id={`dropdown-Primarys-Primary`}
-            title={title}
-            className="location_dropdown_button"
-          >
-            <div className="location_dropdown">
-              <Dropdown.Item onClick={handleSelection}> Select Location </Dropdown.Item>            
-              <Dropdown.Item onClick={handleSelection}> AL </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> AK </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> AZ </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> AR </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> CA </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> CO </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> CT </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> DE </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> FL </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> GA </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> HI </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> ID </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> IL </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> IN </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> IA </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> KS </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> KY </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> LA </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> ME </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> MD </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> MA </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> MI </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> MN </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> MS </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> MO </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> MT </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> NE </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> NV </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> NH </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> NJ </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> NM </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> NY </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> NC </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> ND </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> OH </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> OK </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> OR </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> PA </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> RI </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> SC </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> SD </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> TN </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> TX </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> UT </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> VT </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> VA </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> WA </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> WV </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> WI </Dropdown.Item>
-              <Dropdown.Item onClick={handleSelection}> WY </Dropdown.Item>
-            </div>
-          </DropdownButton>
-        </div>
-    </>
+    <div className="Location Dropdown">
+      <Form.Select
+        aria-label="Location select"
+        name="location"
+        value={props.job}
+        onChange={(e) => {
+          props.setCredentials({
+            ...props.credentials,
+            location: e.target.value,
+          });
+          props.setJob({ ...props.job, location: e.target.value });
+          setTitle(e.target.value);
+        }}
+      >
+        <option>{title}</option>
+        {mapStates}
+      </Form.Select>
+    </div>
   );
 }
 
--- src/pages/MyJobs.js
+++ src/pages/MyJobs.js
@@ -1,57 +1,56 @@
-import { useEffect, useState, useContext } from 'react';
-import JobCard from '../components/JobCard'
-import Navigation from '../components/Navigation'
-import { CurrentUser } from '../context/CurrentUser';
-import Stack from 'react-bootstrap/Stack'
-import Card from 'react-bootstrap/Card'
-
-
+import { useEffect, useState, useContext } from "react";
+import JobCard from "../components/JobCard";
+import Navigation from "../components/Navigation";
+import { CurrentUser } from "../context/CurrentUser";
+import Stack from "react-bootstrap/Stack";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 function MyJobs() {
-    const {currentUser} = useContext(CurrentUser)
-    const [ jobs, setJobs ] = useState([])
-    
-    console.log(currentUser._id)
+  const { currentUser } = useContext(CurrentUser);
+  const [jobs, setJobs] = useState([]);
 
-   
-    
-    
-    useEffect(()=> {
-        const fetchData = async() => {
-            const response = await fetch(`http://localhost:5050/jobs/postedby/${currentUser._id}` , {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-            const resData = await response.json()
-            setJobs(resData)
+  console.log(currentUser._id);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `http://localhost:5050/jobs/postedby/${currentUser._id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        fetchData()
-    },[currentUser._id])
+      );
+      const resData = await response.json();
+      setJobs(resData);
+    };
+    fetchData();
+  }, [currentUser._id]);
 
-
-const children = jobs.map((job)=>{
-    console.log(job)
-    return(
-        <Card style={{margin:'auto'}}>
-            <JobCard data={job}/>
-
-        </Card>
-    ) 
-    }
-)
+  const children = jobs.map((job) => {
+    console.log(job);
+    return (
+      <Card style={{ margin: "auto" }}>
+        <JobCard data={job} />
+      </Card>
+    );
+  });
   return (
     <div>
-        <div style={{marginBottom:'2rem'}}>
-        <Navigation/>
-        </div>
-        <div style={{marginBottom:'2rem'}}>
-        <Stack direction='horizontal' gap={3}>
-        {children}
-
+      <div style={{ marginBottom: "2rem" }}>
+        <Container>
+          <Stack gap={3}>
+            <Navigation />
+          </Stack>
+        </Container>
+      </div>
+      <div style={{ marginBottom: "2rem" }}>
+        <Stack direction="horizontal" gap={3}>
+          {children}
         </Stack>
-        </div>
+      </div>
     </div>
   );
 }
