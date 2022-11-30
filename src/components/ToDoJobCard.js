c3fee0e10178dbe0b31f63a0634a9add11439c16
unknown <kylelarsen12@gmail.com>
2022-11-30 16:38:45 -0500

    Debugging

--- src/components/ToDoJobCard.js
+++ src/components/ToDoJobCard.js
@@ -1,62 +1,57 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { CurrentUser } from '../context/CurrentUser';
-import io from 'socket.io-client'
-import { useState, useContext, useEffect } from 'react'
-import Chat from './Chat';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { CurrentUser } from "../context/CurrentUser";
+import io from "socket.io-client";
+import { useState, useContext, useEffect } from "react";
+import Chat from "./Chat";
 
 function ToDoJobCard(props) {
-    const socket = io.connect('http://localhost:3000/myjobs')
-    const [ showChat, setShowChat ]= useState(false)
-      const {currentUser} = useContext(CurrentUser)
-      
+  const socket = io.connect("http://localhost:3000/myjobs");
+  const [showChat, setShowChat] = useState(false);
+  const { currentUser } = useContext(CurrentUser);
 
-    const handleChatRequest = (e)=>{
-      e.preventDefault()
-      if (currentUser.name !== "" && props.data.id !== ""){
-        console.log(props.data.id)
-          socket.emit('join_room', props.data.id)
-          setShowChat(true)
-          }
-      }
+  const handleChatRequest = (e) => {
+    e.preventDefault();
+    if (currentUser.name !== "" && props.data.id !== "") {
+      console.log(props.data.id);
+      socket.emit("join_room", props.data.id);
+      setShowChat(true);
+    }
+  };
 
-const hideChat=()=>{
-    setShowChat(false)
-}
-
+  const hideChat = () => {
+    setShowChat(false);
+  };
 
   return (
     <div>
-      {showChat ? 
-      <Card style={{ width: '20em', height: '30rem',justifyContent:'center'}}>
-        <Chat socket={socket} username={currentUser.name} room={currentUser._id}/>
-        <Button variant="warning" onClick={hideChat}>Close Chat</Button>
-      </Card>
-        :
-     <Card style={{ width: '20rem', height: '30rem', }}>
-         <Card.Img variant="bottom" src="holder.js/100px180" />
-         <div style={{marginTop: '10rem'}}>
-         <Card.Title>{props.data.name}</Card.Title>
-       <Card.Body>
-         <Card.Text>
-           Category: {props.data.category}
-         </Card.Text>
-         <Card.Text>
-           Location: {props.data.location}
-         </Card.Text>
-         <Card.Text>
-           Job Description:{props.data.description}
-         </Card.Text>      
-          <Card.Text>
-           Job Accepted By:
-         </Card.Text>
-         <Button variant="warning" onClick={handleChatRequest}>Chat</Button> 
-        </Card.Body>
-         </div>
-     </Card>
-     }
+      {showChat ? (
+        <Card
+          style={{ width: "20em", height: "30rem", justifyContent: "center" }}
+        >
+          <Chat username={currentUser.name} room={currentUser._id} />
+          <Button variant="warning" onClick={hideChat}>
+            Close Chat
+          </Button>
+        </Card>
+      ) : (
+        <Card style={{ width: "20rem", height: "30rem" }}>
+          <Card.Img variant="bottom" src="holder.js/100px180" />
+          <div style={{ marginTop: "10rem" }}>
+            <Card.Title>{props.data.name}</Card.Title>
+            <Card.Body>
+              <Card.Text>Category: {props.data.category}</Card.Text>
+              <Card.Text>Location: {props.data.location}</Card.Text>
+              <Card.Text>Job Description:{props.data.description}</Card.Text>
+              <Card.Text>Job Accepted By:</Card.Text>
+              <Button variant="warning" onClick={handleChatRequest}>
+                Chat
+              </Button>
+            </Card.Body>
+          </div>
+        </Card>
+      )}
     </div>
-  
   );
 }
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,57 +2,55 @@ import Container from "react-bootstrap/Container";
 import Navigation from "../components/Navigation";
 import Stack from "react-bootstrap/Stack";
 import { useNavigate } from "react-router-dom";
-import { setImageColor, createImageFromInitials } from "../components/Utilities"
+import {
+  setImageColor,
+  createImageFromInitials,
+} from "../components/Utilities";
 import { CurrentUser } from "../context/CurrentUser";
 import { useContext } from "react";
 
 function Profile() {
-    const { currentUser } = useContext(CurrentUser);
-    const name = currentUser.name;
-    const imgSrc = "";
-    const navigate = useNavigate();
+  const { currentUser } = useContext(CurrentUser);
+  const name = currentUser.name;
+  const imgSrc = "";
+  const navigate = useNavigate();
 
-    function clearStorage() {
-        // Clear localStorage token 
-        localStorage.clear();
-        navigate("/");
-        window.location.reload(false);
-      }
+  function clearStorage() {
+    // Clear localStorage token
+    localStorage.clear();
+    navigate("/");
+    window.location.reload(false);
+  }
 
   return (
     <div>
       <Container>
         <Stack gap={3}>
           <Navigation />
-          <ProfileTile/>
         </Stack>
-    <div>
-
-        <img
+        <div>
+          <img
             className="profile_pic"
-            id='preview'
+            id="preview"
             src={
-                imgSrc.length <= 0
-                    ? createImageFromInitials(500, name, setImageColor())
-                    : imgSrc
+              imgSrc.length <= 0
+                ? createImageFromInitials(500, name, setImageColor())
+                : imgSrc
             }
-            alt='profile-pic'
-        />
-        <div className="top-Home-Page">
-        <h2>About Me</h2>
-        <p>{currentUser.name}</p>
-        <p>{currentUser.location}</p>
-        <p>{currentUser.name}</p>
-          <div class="form-group">
-
-            <button onClick={clearStorage}>Log out</button>
+            alt="profile-pic"
+          />
+          <div className="top-Home-Page">
+            <h2>About Me</h2>
+            <p>{currentUser.name}</p>
+            <p>{currentUser.location}</p>
+            <p>{currentUser.name}</p>
+            <div class="form-group">
+              <button onClick={clearStorage}>Log out</button>
+            </div>
           </div>
         </div>
-    </div>
       </Container>
     </div>
-
   );
-
 }
 export default Profile;
