2be0d809651612d425f6b29eb4bffa672f83463e
unknown <kylelarsen12@gmail.com>
2022-11-30 01:00:57 -0500

    Update button on card to show provider when user takes job

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,92 +1,112 @@
 import Navigation from "../components/Navigation";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router";
 import Form from "react-bootstrap/Form";
 import Stack from "react-bootstrap/Stack";
 import Container from "react-bootstrap/Container";
 import "../jobs.css";
-
+import { CurrentUser } from "../context/CurrentUser";
 
 /**@TODO fix selection lagging */
 
 const Jobs = () => {
+  const navigate = useNavigate();
+  const { currentUser } = useContext(CurrentUser);
+  const [allJobs, setAllJobs] = useState([]);
+  const [title, setTitle] = useState("");
+  const [endpoint, setEndpoint] = useState("");
+  const [provider, setProvider] = useState({ provider: "" });
 
-    const [allJobs, setAllJobs] = useState([]);
-    const [title, setTitle] = useState("");
-    const [endpoint, setEndpoint] = useState("");
+  const url = `http://localhost:5050/jobs/` + endpoint;
 
-    const url = `http://localhost:5050/jobs/` + endpoint;
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(url);
-            const resData = await response.json();
-            setAllJobs(resData);
-        };
-        fetchData();
-    }, [url]);
-
-    const handleSelection = (e) => {
-        setTitle(e.target.value);
-        console.log(title);
-        if (title === "homeCleaning") {
-            setEndpoint("category/homeCleaning");
-            console.log(title);
-        } else if (title === "petCare") {
-            setEndpoint("category/petCare");
-        } else if (title === "landscaping") {
-            setEndpoint("category/landscaping");
-        } else {
-            setEndpoint("");
-            console.log("not found");
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const resData = await response.json();
+      setAllJobs(resData);
     };
-    const children = allJobs.map((job) => {
-        if (job.notCompleted) {
-            return (
-                <div key={job.id}>
-                    <Card style={{ width: "18rem" }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>{job.name}</Card.Title>
-                            <span className="category">
-                                <Card.Text>
-                                    Job Description: {job.category}
-                                </Card.Text>
-                            </span>
-                            <Card.Text>requested by: {job.postedBy}</Card.Text>
-                            <Card.Text>Location: {job.location}</Card.Text>
-                            <Button variant="primary">Request Work</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-            );
-        } else {
-            return <div> No jobs at the moment. Try different search</div>;
-        }
-    });
+    fetchData();
+  }, [url]);
 
-    return (
+  const handleSelection = (e) => {
+    setTitle(e.target.value);
+    console.log(title);
+    if (title === "homeCleaning") {
+      setEndpoint("category/homeCleaning");
+      console.log(title);
+    } else if (title === "petCare") {
+      setEndpoint("category/petCare");
+    } else if (title === "landscaping") {
+      setEndpoint("category/landscaping");
+    } else {
+      setEndpoint("");
+      console.log("not found");
+    }
+  };
+
+  //Updates job provider to currently logged in user
+  const handleRequestWork = async (job) => {
+    try {
+      const response = await fetch(`http://localhost:5050/jobs/${job._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(currentUser),
+      });
+      const data = await response.json();
+      setProvider(data);
+      navigate("/profile");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const children = allJobs.map((job, i) => {
+    if (job.notCompleted) {
+      return (
         <div>
-            <Container>
-                <Stack gap={3}>
-                    <Navigation />
-                </Stack>
-                Search by Category:
-                <Form onChange={handleSelection}>
-                    <Form.Select aria-label="Default select example">
-                        <option>Open this select menu</option>
-                        <option value="landscaping">Landscaping</option>
-                        <option value="homeCleaning">Home Cleaning</option>
-                        <option value="petCare">Pet Care</option>
-                    </Form.Select>
-                </Form>
-                <div className="jobChildren">{children}</div>
-            </Container>
+          <Card style={{ width: "18rem" }} key={`Card ${i}`}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>{job.name}</Card.Title>
+              <span className="category">
+                <Card.Text>Job Description: {job.category}</Card.Text>
+              </span>
+              <Card.Text>requested by: {job.postedBy}</Card.Text>
+              <Card.Text>Location: {job.location}</Card.Text>
+              <Button variant="primary" onClick={() => handleRequestWork(job)}>
+                Request Work
+              </Button>
+            </Card.Body>
+          </Card>
         </div>
-    );
+      );
+    } else {
+      return <div> No jobs at the moment. Try different search</div>;
+    }
+  });
 
-  
+  return (
+    <div>
+      <Container>
+        <Stack gap={3}>
+          <Navigation />
+        </Stack>
+        Search by Category:
+        <Form onChange={handleSelection}>
+          <Form.Select aria-label="Default select example">
+            <option>Open this select menu</option>
+            <option value="landscaping">Landscaping</option>
+            <option value="homeCleaning">Home Cleaning</option>
+            <option value="petCare">Pet Care</option>
+          </Form.Select>
+        </Form>
+        <div className="jobChildren">{children}</div>
+      </Container>
+    </div>
+  );
 };
 export default Jobs;
